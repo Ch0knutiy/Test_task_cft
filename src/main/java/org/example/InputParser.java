8d9f43a54d55fa2e358dffcfa400fa83baf0f1d3
George Ustyanchik <ghkro123@gmail.com>
2023-07-27 03:11:47 +0700

    add args parser

---
+++ src/main/java/org/example/InputParser.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class InputParser {
+    private String outFileName;
+    private ArrayList<String> inputFileNames;
+    private boolean isString;
+    private boolean isReverse;
+
+    public InputParser(){
+        isString = false;
+        isReverse = false;
+    }
+
+    public boolean parse(String[] args){
+        int argsCounter = 0;
+        if(args.length >=3){
+            for(int i = 0; i < 2; i++) {
+                if ("-a".equalsIgnoreCase(args[argsCounter]) || "-d".equalsIgnoreCase(args[argsCounter])) {
+                    if ("-d".equalsIgnoreCase(args[argsCounter])) {
+                        isReverse = true;
+                    }
+                    argsCounter++;
+                }
+
+                if ("-s".equalsIgnoreCase(args[argsCounter]) || "-i".equalsIgnoreCase(args[argsCounter])) {
+                    if ("-s".equalsIgnoreCase(args[argsCounter])) {
+                        isString = true;
+                    }
+                    argsCounter++;
+                }
+            }
+            outFileName = args[argsCounter++];
+            inputFileNames = new ArrayList<String>(Arrays.stream(args).toList().subList(argsCounter, args.length));
+            return inputFileNames.size() >= 1;
+        }
+        else return false;
+    }
+
+    public String getOutFileName(){
+        return outFileName;
+    }
+
+    public ArrayList<String> getInputFileNames() {
+        return inputFileNames;
+    }
+
+    public boolean isReverse() {
+        return isReverse;
+    }
+
+    public boolean isString() {
+        return isString;
+    }
+}
