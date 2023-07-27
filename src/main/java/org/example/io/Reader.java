cb89a04d4f1543e03c323613bc67d18f3fa7c9db
George Ustyanchik <ghkro123@gmail.com>
2023-07-27 18:47:31 +0700

    add reader, writer

--- src/main/java/org/example/io/Reader.java
+++ src/main/java/org/example/io/Reader.java
@@ -1,4 +1,32 @@
 package org.example.io;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Reader {
+    private String value;
+    private Scanner scanner;
+    private boolean empty = false;
+    public Reader(File file){
+        try{
+            scanner = new Scanner(file);
+            if(scanner.hasNext())
+                value = scanner.next();
+            else{
+                empty = true;
+                System.out.println(file.getName() + " is empty");
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+            empty = true;
+        }
+    }
+    public void read(boolean isString, boolean isReverse){
+        if(scanner.hasNext())
+            value = scanner.next();
+        else
+            empty = true;
+    }
 }
--- src/main/java/org/example/io/Writer.java
+++ src/main/java/org/example/io/Writer.java
@@ -1,4 +1,29 @@
 package org.example.io;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Writer {
+    FileWriter writer;
+    public Writer(File file) {
+        try {
+            writer = new FileWriter(file);
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void write(String str){
+        if (str == null)
+            return;
+        try{
+            writer.write(str + "\n");
+            writer.flush();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
